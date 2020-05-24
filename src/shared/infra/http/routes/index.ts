3c3a1e7b2e339378bd4b1a18c7fb70ff716541e4
Gustavo <graf.sousa@gmail.com>
2020-05-24 15:58:23 -0300

    [feature]: Save token in BD

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import { usersRouter } from '@modules/users/infra/http/routes/user.routes';
 import { sessionRouter } from '@modules/users/infra/http/routes/sessions.routes';
 import { appointmentsRouter } from '@modules/appointments/infra/http/routes/appointments.routes';
+import { passwordRouter } from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 export { routes };
