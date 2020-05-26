2155fdcafb7160106d9d6384bf35f75ff0b1db9e
Gustavo <graf.sousa@gmail.com>
2020-05-26 20:44:22 -0300

    [feature]: Add list providers

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { User } from '../infra/typeorm/entities/User';
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
+import { IFindAllProvidersDTO } from '../dtos/IFindAllProvidersDTO';
 
 export interface IUsersRepository {
+  findAllProvider(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import { usersRouter } from '@modules/users/infra/http/routes/user.routes';
 import { sessionRouter } from '@modules/users/infra/http/routes/sessions.routes';
 import { appointmentsRouter } from '@modules/appointments/infra/http/routes/appointments.routes';
+import { providersRouter } from '@modules/appointments/infra/http/routes/providers.routes';
 import { passwordRouter } from '@modules/users/infra/http/routes/password.routes';
 import { profileRouter } from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRouter);
 routes.use('/password', passwordRouter);
